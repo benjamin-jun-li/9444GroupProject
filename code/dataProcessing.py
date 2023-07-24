976a098ce59406708638b5223f3f8affab002ea6
humbertPeng <134574286+humbertPeng@users.noreply.github.com>
2023-07-25 04:03:01 +0800

    Add files via upload

--- code/dataProcessing.py
+++ code/dataProcessing.py
@@ -2,10 +2,13 @@ import os
 import glob
 import re
 import nltk
+import torch
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from gensim.models import Word2Vec
 import numpy as np
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
 
 # Get the data, the source is sited.
 # @InProceedings{maas-EtAl:2011:ACL-HLT2011,
@@ -14,12 +17,22 @@ import numpy as np
 #   booktitle = {Proceedings of the 49th Annual Meeting of the Association for Computational Linguistics: Human Language Technologies},
 #   month     = {June},
 #   year      = {2011},
-#   address   = {Portland, Oregon, USA},
+#   address   = {Portland, Oregon, USA},pi
 #   publisher = {Association for Computational Linguistics},
 #   pages     = {142--150},
 #   url       = {http://www.aclweb.org/anthology/P11-1015}
 # }
 
+class VectorDataset(Dataset):
+    def __init__(self, data_list, target_list):
+        self.data_list = data_list
+        self.target_list = target_list
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx):
+        return self.data_list[idx], self.target_list[idx]
 
 def load_data(directory):
     texts = []
@@ -38,10 +51,10 @@ def load_data(directory):
 
 # Download the NLTK data package
 
-nltk.download('omw-1.4')
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('stopwords')
+# nltk.download('omw-1.4')
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
 
 # Initialising word reducers and deactivators
 
@@ -76,3 +89,75 @@ def text_to_vec(text, model):
     words = text.split()
     word_vecs = [model.wv[word] for word in words if word in model.wv.key_to_index]
     return np.mean(word_vecs, axis=0)
+
+
+# For the ConvLstm
+def text_to_vec_con(text, model, max_seq_length):
+    words = text.split()
+    word_vecs = []
+    for word in words:
+        if word in model.wv.key_to_index:
+            word_vecs.append(model.wv[word])
+    for i in range(max_seq_length - len(words)):
+        word_vecs.append(np.zeros(model.vector_size))
+    return np.array(word_vecs[:max_seq_length])
+
+# Load data
+train_texts, train_labels = load_data('../aclImdb_data/train')
+test_texts, test_labels = load_data('../aclImdb_data/test')
+
+# Preprocess texts
+train_texts = preprocess_text(train_texts)
+test_texts = preprocess_text(test_texts)
+
+
+np.save('../p_data/train_texts.npy', train_texts)
+np.save('../p_data/train_labels.npy', train_labels)
+np.save('../p_data/test_texts.npy', test_texts)
+np.save('../p_data/test_labels.npy', test_labels)
+
+
+
+#
+# # Convert texts to vectors
+# train_data = [text_to_vec(text, w2v_model) for text in train_texts]
+# test_data = [text_to_vec(text, w2v_model) for text in test_texts]
+#
+# # Divide the training set and validation set
+# train_data, val_data, train_labels, val_labels = train_test_split(
+#     train_data, train_labels, test_size=0.2, random_state=42)
+#
+# # Convert lists to tensors
+# train_data = [torch.tensor(vec) for vec in train_data]
+# val_data = [torch.tensor(vec) for vec in val_data]
+# test_data = [torch.tensor(vec) for vec in test_data]
+#
+# np.save('train_data.npy', train_data)
+# np.save('train_labels.npy', train_labels)
+# np.save('val_data.npy', val_data)
+# np.save('val_labels.npy', val_labels)
+# np.save('test_data.npy', test_data)
+# np.save('test_labels.npy', test_labels)
+
+# # For conv_lstm
+# max_length = 1519
+# train_data_conv = [text_to_vec_con(text, w2v_model, max_length) for text in train_texts]
+# test_data_conv = [text_to_vec_con(text, w2v_model, max_length) for text in test_texts]
+#
+# # Divide the training set and validation set
+# train_data_conv, val_data_conv, train_labels_conv, val_labels_conv = train_test_split(
+#     train_data_conv, train_labels, test_size=0.2, random_state=42)
+#
+# # Convert lists to tensors
+# train_data_conv = [torch.tensor(vec).unsqueeze(0) for vec in train_data_conv]
+# val_data_conv = [torch.tensor(vec).unsqueeze(0) for vec in val_data_conv]
+# test_data_conv = [torch.tensor(vec).unsqueeze(0) for vec in test_data_conv]
+#
+# # Create DataLoader
+# train_loader_conv = DataLoader(VectorDataset(train_data_conv, train_labels), batch_size)
+# val_loader_conv = DataLoader(VectorDataset(val_data_conv, val_labels), batch_size)
+# test_loader_conv = DataLoader(VectorDataset(test_data_conv, test_labels), batch_size)
+#
+# np.save('../p_data/train_loader_conv.npy', np.array(train_loader_conv))
+# np.save('../p_data/val_loader_conv.npy', np.array(val_loader_conv))
+# np.save('../p_data/test_loader_conv.npy', np.array(test_loader_conv))
